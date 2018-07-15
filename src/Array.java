b63e1378abd7e5345481fb52d1db84564ce7d9f5
hmustermann <Sebawe80@gmx.de>
2018-07-16 00:46:05 +0200

    first commit

---
+++ src/Array.java
@@ -0,0 +1,14 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] beispiel = new int[] {3, 4, 5, 6, 2, 23, 19};
+		
+		for(int i=0; i<beispiel.length; i++){
+			System.out.println(beispiel[i] + " || " +beispiel.length);
+		}
+		
+
+	}
+
+}
